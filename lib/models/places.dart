2e012f15e3bf8fc88cda69449895650a68330653
djdeo <deoseoer@gmail.com>
2020-12-23 11:43:52 +0800

    feat: add screen route

---
+++ lib/models/places.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/foundation.dart';
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation({
+    @required this.latitude,
+    @required this.longitude,
+    this.address,
+  });
+}
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final String image;
+
+  Place({
+    @required this.id,
+    @required this.title,
+    @required this.location,
+    @required this.image,
+  });
+}
