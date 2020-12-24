c89ff938a8d734b7bc0ebd4311b23bfb4a38993f
djdeo <deoseoer@gmail.com>
2020-12-24 10:15:24 +0800

    feat: render places list

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './add_place_screen.dart';
+import '../providers/great_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Places List'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Places List'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              },
+            )
+          ],
+        ),
+        body: Consumer<GreatPlaces>(
+          child: Center(
+            child: const Text('Currently no places yet, add one? '),
+          ),
+          builder: (ctx, greatPlaces, ch) {
+            return greatPlaces.items.length <= 0
+                ? ch
+                : ListView.builder(
+                    itemCount: greatPlaces.items.length,
+                    itemBuilder: (ctx, i) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage:
+                              FileImage(greatPlaces.items[i].image),
+                        ),
+                        title: Text(greatPlaces.items[i].title),
+                        onTap: () {},
+                      );
+                    },
+                  );
+          },
+        ));
   }
 }
