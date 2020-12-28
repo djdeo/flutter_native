cf29e11a21f91a8ae573abf2eee92b4ddc940193
djdeo <deoseoer@gmail.com>
2020-12-28 15:08:26 +0800

    feat: add current location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart' as flutterMap;
 import "package:latlong/latlong.dart" as latLng;
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -8,6 +9,66 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  double _lat;
+  double _lng;
+
+  Widget _showMapContainer(double lat, double lng) {
+    setState(() {
+      _lat = lat;
+      _lng = lng;
+    });
+
+    return Container(
+      height: 170,
+      child: flutterMap.FlutterMap(
+        options: flutterMap.MapOptions(
+          center: latLng.LatLng(lat, lng),
+          zoom: 13.0,
+        ),
+        layers: [
+          flutterMap.TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c'],
+          ),
+          flutterMap.MarkerLayerOptions(
+            markers: [
+              flutterMap.Marker(
+                width: 80.0,
+                height: 80.0,
+                point: latLng.LatLng(lat, lng),
+                builder: (ctx) => Container(
+                  child: FlutterLogo(
+                    size: 20.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      final locData = await Location().getLocation();
+      print(locData.latitude);
+      print(locData.longitude);
+      _showMapContainer(locData.latitude, locData.longitude);
+    } catch (error) {
+      print(error);
+      return;
+    }
+  }
+
+  // Future<void> _selectOnMap() async {
+  //   final selectedLocation =
+  //       await Navigator.of(context).push(MaterialPageRoute(
+  //     fullscreenDialog: true,
+  //     // builder: (ctx) => MapScreen()
+  //   ));
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,45 +80,18 @@ class _LocationInputState extends State<LocationInput> {
           decoration: BoxDecoration(
             border: Border.all(width: 1, color: Colors.grey),
           ),
-          child: Container(
-            height: 170,
-            child: flutterMap.FlutterMap(
-              options: flutterMap.MapOptions(
-                  center: latLng.LatLng(30.67, 104.07),  // chengdu 
-                  zoom: 13.0,
-              ),
-              layers: [
-                flutterMap.TileLayerOptions(
-                  urlTemplate:
-                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c'],
-                ),
-                flutterMap.MarkerLayerOptions(
-                  markers: [
-                    flutterMap.Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: latLng.LatLng(51.5, -0.09),
-                      builder: (ctx) => Container(
-                        child: FlutterLogo(
-                          size: 50.0,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          child: _lat != null
+              ? _showMapContainer(_lat, _lng)
+              : Text('No place selected'),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FlatButton.icon(
               icon: Icon(Icons.location_on),
-              label: Text('Locate Current Spot'),
+              label: Text('Use Current'),
               textColor: Theme.of(context).primaryColor,
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
             ),
             FlatButton.icon(
               icon: Icon(Icons.map),
